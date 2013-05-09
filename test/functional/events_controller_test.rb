61f61cb222bf24a89975a912a0802305a68e4700
Geoffrey Thompson <fleaplus@gmail.com>
2013-05-08 21:00:39 -0600

    fixed tests, changed dateRange select

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -18,7 +18,7 @@ class EventsControllerTest < ActionController::TestCase
 
   test "should create event" do
     assert_difference('Event.count') do
-      post :create, event: { log: @event.log, punchtime: @event.punchtime, type: @event.type }
+      post :create, event: { log: @event.log, punchtime: @event.punchtime, punch_type: @event.punch_type }
     end
 
     assert_redirected_to event_path(assigns(:event))
@@ -35,7 +35,7 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should update event" do
-    put :update, id: @event, event: { log: @event.log, punchtime: @event.punchtime, type: @event.type }
+    put :update, id: @event, event: { log: @event.log, punchtime: @event.punchtime, punch_type: @event.punch_type }
     assert_redirected_to event_path(assigns(:event))
   end
 
